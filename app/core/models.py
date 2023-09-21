310a93e26072dada65ee371ed4045a0aa05d8dc3
Daniil Kulbatski <daniil.kulbackiy@gmail.com>
2023-09-21 17:39:20 +0200

    create models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'category'
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'city'
+
+class Advert(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    city = models.ForeignKey(City, related_name='city_adverts', on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, related_name='category_adverts')
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = 'advert'
